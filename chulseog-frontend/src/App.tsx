425b41fdbd6a206fa2f07494c48c790cbf3a6a60
fer-hnndz <jfernandohernandez28@gmail.com>
2025-07-15 17:00:25 -0600

    Installed HeroUI component library on frontend

--- chulseog-frontend/src/App.tsx
+++ chulseog-frontend/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Alert, Button } from "@heroui/react";
+import "./index.css";
+import { HeroUIProvider } from "@heroui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HeroUIProvider>
+        <div>
+          <Button color="danger">Peligro</Button>
+          <Alert title="Chulseog" description="Welcome" />
+          <h1 className="font-extrabold text-red-500 text-2xl">asd</h1>
+        </div>
+      </HeroUIProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
