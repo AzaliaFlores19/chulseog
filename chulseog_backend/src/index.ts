613dfddda059221eff9b0bf1762ebdf17d05bf1a
fer-hnndz <jfernandohernandez28@gmail.com>
2025-10-07 00:15:04 -0600

    added person route

--- chulseog_backend/src/index.ts
+++ chulseog_backend/src/index.ts
@@ -1,14 +1,24 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import express from "express";
+import cors from "cors";
+import { personTable } from "./db/schema";
 
 const app = express();
 const EXPRESS_PORT = 3000;
 
 const db = drizzle(process.env.DATABASE_URL!);
 
+app.use(cors());
+
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
+});
+
+app.get("/person", async (req: express.Request, res: express.Response) => {
+  const persons = await db.select().from(personTable);
+  console.log("Retrieved persons:", persons);
+  res.json(persons);
 });
 
 app.listen(EXPRESS_PORT, () => {
