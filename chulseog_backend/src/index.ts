e2f0362cdbf66631334b14059dfc737fe137613b
fer-hnndz <jfernandohernandez28@gmail.com>
2025-10-07 00:00:19 -0600

    added base express app on backend

--- chulseog_backend/src/index.ts
+++ chulseog_backend/src/index.ts
@@ -1,4 +1,16 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import express from "express";
+
+const app = express();
+const EXPRESS_PORT = 3000;
 
 const db = drizzle(process.env.DATABASE_URL!);
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Server is running at http://localhost:${EXPRESS_PORT}`);
+});
